55d8b5d84153f44be0856b4b63842944dcf434be
QT_BESAU <quoctinhstudy1911@gmail.com>
2026-05-14 11:45:29 +0700

    WIP: review feature

--- dormitory_backend/src/main/java/com/stu/dormitory/common/exception/AppException.java
+++ dormitory_backend/src/main/java/com/stu/dormitory/common/exception/AppException.java
@@ -1,8 +1,31 @@
 package com.stu.dormitory.common.exception;
 
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+import java.net.http.HttpRequest;
+
+// Dùng để tạo lỗi riêng cho hệ thống dùng kế thừa từ bên RuntimeException ( Không có throw và try- catch )
+@Getter
 public class AppException extends RuntimeException{
-    public  AppException(String message)
-    {
+    private final HttpStatus status;
+
+    public AppException(String message, HttpStatus status) {
         super(message);
+        this.status = status;
     }
 }
+/**
+ * Kiến thức ta đã sử dụng tính kế thùa từ lớp RuntimeException và phát triển 1 cái là status
+ * Tác dụng khi mà có lỗi ta cần tạo thêm biến lỗi và status nhen
+ */
+/**
+ * Nên ghi lỗi bằng tiếng anh sao này đễ debug cho team
+ * Cách sử dụng ví dụ
+ * Nếu mà ta có lỗi là Phòng đã đầy sinh viên rồi thì ta có thể dùng là
+ *  if( room.isFull())
+ *  {
+ *      throw new AppException("Room is full", HttpStatus.BAD_REQUEST)
+ *  }
+ */
+
--- dormitory_backend/src/main/java/com/stu/dormitory/common/exception/GlobalExceptionHandler.java
+++ dormitory_backend/src/main/java/com/stu/dormitory/common/exception/GlobalExceptionHandler.java
@@ -1,22 +1,60 @@
 package com.stu.dormitory.common.exception;
 
 import com.stu.dormitory.common.response.ApiResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestControllerAdvice
+/**
+ * Này là flow của bắt lỗi
+ * Controller
+ *    ↓
+ * Service
+ *    ↓
+ * throw Exception
+ *    ↓
+ * GlobalExceptionHandler bắt
+ *    ↓
+ * Trả JSON lỗi về frontend
+ */
+
+@RestControllerAdvice // Annotation bắt exception toàn bộ project nha
 public class GlobalExceptionHandler {
+
+    /**
+     * Quy tắc chung nè
+     throw new AppException("Room is full")
+     ↓
+     Spring bắt được
+     ↓
+     đưa exception vào biến ex
+     */
 
     @ExceptionHandler(AppException.class)
     public ResponseEntity<ApiResponse<?>> handleAppException(AppException ex) {
-        return ResponseEntity.badRequest()
-                .body(new ApiResponse<>(false, ex.getMessage(), null));
+      return ResponseEntity
+                .status(ex.getStatus())
+                .body(new ApiResponse<>(
+                      false,
+                      ex.getMessage(),
+                      null));
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ApiResponse<?>> handleException(Exception ex) {
-        return ResponseEntity.internalServerError()
-                .body(new ApiResponse<>(false, "Lỗi hệ thống", null));
+
+        // INTERNAL_SERVER_ERROR: 500
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new ApiResponse<>(
+                        false,
+                        "Lỗi hệ thống",
+                        null
+                ));
     }
 }
+/**
+ * Kiến thức ResponseEntity đại diện cho HTTP Response
+ * Chứa status Header và body (json)
+ */
