a7a71fefbc7582cd82d452cff744fbefb0663f91
QT_BESAU <quoctinhstudy1911@gmail.com>
2026-05-14 18:53:09 +0700

    clean main

--- dormitory_backend/src/main/java/com/stu/dormitory/modules/application/enums/PeriodStatus.java
+++
@@ -1,9 +0,0 @@
-package com.stu.dormitory.modules.application.enums;
-
-/**
- * Trạng thái của kỳ đăng ký KTX
- */
-public enum PeriodStatus {
-    OPEN, // Mở đăng ký
-    CLOSED // Đóng đăng ký
-}
--- dormitory_backend/src/main/java/com/stu/dormitory/modules/auth/enums/Role.java
+++
@@ -1,7 +0,0 @@
-package com.stu.dormitory.modules.auth.enums;
-
-public enum Role {
-    USER, // Sinh viên
-    STAFF, // Nhân viên quản lý
-    ADMIN // Quản trị viên
-}
